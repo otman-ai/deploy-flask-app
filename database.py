85fe0814576935c75b0dbe0f751c72c52e8c474c
otman heddouch <otmanheddouchai@gmail.com>
2023-09-17 01:14:44 +0200

    Add login and Sign up Function

--- database.py
+++ database.py
@@ -1,16 +1,24 @@
 from sqlalchemy import create_engine , text
-host = "localhost"
-user = "root"
-password = "pscale_pw_vc0Uq1jSdgv4UIJ9DSIND0g9iKBdPWL3xVp65NVek6A"
+# host = "localhost"
+# user = "root"
+# password = "pscale_pw_vc0Uq1jSdgv4UIJ9DSIND0g9iKBdPWL3xVp65NVek6A"
 
-database = "users"
+# database = "users"
+# connection_db = f"mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8mb4"
+# connect_args={
+#         "ssl": {
+#             "ssl_ca": "/etc/ssl/cert.pem"
+#         }
+#     }
+# engine = create_engine(connection_db,connect_args=connect_args)
+
+host = "aws.connect.psdb.cloud"
+user = "nljrirmxnsmw6u0ix81u"
+password = "pscale_pw_JylvFWxmkFQPK38mER6I2clQDvvSf2UETFzFleNaX4l"
+database = "data-users"
 connection_db = f"mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8mb4"
-connect_args={
-        "ssl": {
-            "ssl_ca": "/etc/ssl/cert.pem"
-        }
-    }
-engine = create_engine(connection_db,connect_args=connect_args)
+connect_args = {"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}}
+engine = create_engine(connection_db, connect_args=connect_args)
 
 def load_data():
     with engine.connect() as conn:
