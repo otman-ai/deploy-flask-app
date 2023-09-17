20856ce782e48c26484d02f20622a2e3c903ae46
otman heddouch <otmanheddouchai@gmail.com>
2023-09-17 02:39:07 +0200

    Initial commit

--- database.py
+++ database.py
@@ -13,8 +13,8 @@ from sqlalchemy import create_engine , text
 # engine = create_engine(connection_db,connect_args=connect_args)
 
 host = "aws.connect.psdb.cloud"
-user = "2o7vv2sr9vpxz3vgl3fe"
-password = "pscale_pw_OaUmNu8hB0RSYfjz8Culjs8dUx9kgb0VPwAxN77lWx3"
+user = "b4bqjoa5liek9o4gu85f"
+password = "pscale_pw_ft3lt1rdkRQX8m1plmljY0T6D31bWAv8uuWLT3Sjvut"
 database = "data-users"
 connection_db = f"mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8mb4"
 connect_args = {"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}}
