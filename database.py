89c767fe67c76c72bc16027c21dab8945a4261b1
otman heddouch <otmanheddouchai@gmail.com>
2023-09-23 00:12:16 +0200

    Add constant.py

--- database.py
+++ database.py
@@ -1,37 +1,44 @@
 from sqlalchemy import create_engine , text
 import os
-# host = "localhost"
-# user = "root"
-# password = "pscale_pw_vc0Uq1jSdgv4UIJ9DSIND0g9iKBdPWL3xVp65NVek6A"
-
-# database = "users"
-# connection_db = f"mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8mb4"
-# connect_args={
-#         "ssl": {
-#             "ssl_ca": "/etc/ssl/cert.pem"
-#         }
-#     }
 connection_db = os.environ['DB_CONNECTION_STRING']
 
 connect_args = {"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}}
 engine = create_engine(connection_db, connect_args=connect_args)
 
-def load_data():
-    with engine.connect() as conn:
-        query = "SELECT id, username, email, password_hash FROM users"
-        result = conn.execute(text(query))
-        data = result.all()
-        
-        results = [{
-            'id': row[0],
-            'username': row[1],
-            'email': row[2],
-            'password': row[3]
-        } for row in data]
-        
-        return results
 
-    
+def load_data_condition(id):
+    with engine.connect() as conn:
+        query = "SELECT * FROM cameras where id= :id"
+        result = conn.execute(text(query), parameters={"id":id})
+        data = result.all()
+        if data != []:
+            return data[0][2]
+
+def load_data(username):
+    user_id = get_user_id(username)
+    with engine.connect() as conn:
+        query = "SELECT * FROM cameras where user_id= :user_id"
+        result = conn.execute(text(query), parameters={"user_id":user_id})
+        data = result.all()
+        dict_data = [{
+            "id":row[0],
+            "camera_ipAdress":row[2],
+            "camera_date":row[3],
+            "camera_name":row[4],
+            "video_saved_path":row[5]
+        } for row in data]
+
+        return dict_data
+
+
+def get_user_id(username):
+    with engine.connect() as conn:
+        query = "SELECT * FROM users where username= :username"
+        result = conn.execute(text(query), parameters={"username":username})
+        data = result.all()
+        if len(data) >=1:
+            return data[0][0]
+
 def check_if_exist(username, email):
     email_exist = False
     username_exist = False
@@ -62,5 +69,26 @@ def login_function(username):
         
     return result
 
-print(login_function("wissal"))
-# check_if_exist("wissal","wissal@gmail.com")
+
+def insert_to_camera(user_id,camera_ipAdress,camera_date):
+    parametres = {"user_id":user_id, "camera_ipAdress":camera_ipAdress, "camera_date":camera_date}
+    QUERY = text("INSERT INTO cameras  (user_id, camera_ipAdress, camera_date ) VALUES (:user_id, :camera_ipAdress, :camera_date)")
+    with engine.connect() as conn:
+        result = conn.execute(QUERY,parameters=parametres)
+        conn.commit()
+        if result.rowcount > 0:
+            result_2 = conn.execute(text("SELECT * FROM cameras where camera_ipAdress=:camera_ipAdress"),
+                                    parameters={"camera_ipAdress":camera_ipAdress}).all()
+            id = result_2[0][0]
+            return id
+        return False
+    
+def insert_to_camera_part(camera_name ,video_saved_path ):
+    QUERY = text("UPDATE cameras SET camera_name=:camera_name, video_saved_path =:video_saved_path WHERE id= LAST_INSERT_ID();")
+    with engine.connect() as conn:
+        result = conn.execute(QUERY,parameters={'camera_name':camera_name, 'video_saved_path':video_saved_path})
+        conn.commit()
+        if result.rowcount > 0:
+            return True
+        return False
+
