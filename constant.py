327da0028fe4c078ff1882381b6c0c0f9dcd1455
otman heddouch <otmanheddouchai@gmail.com>
2023-09-23 00:04:22 +0200

    Add constant.py

---
+++ constant.py
@@ -0,0 +1,21 @@
+
+#Classes
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+                  "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+                  "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+                  "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+                  "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+                  "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+                  "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+                  "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+                  "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+                  "teddy bear", "hair drier", "toothbrush"
+                  ]
+# columns for tracked data
+columns = ["Object_name","Time","id"]
+# Setup the videos path and data path storage
+UPLOAD_FOLDER = "static/videos/"
+UPLOAD_FOLDER_DATA = "data/"
+
+
+ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mkv', 'mov'}  # Add more extensions if needed
